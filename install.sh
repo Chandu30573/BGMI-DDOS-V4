607dfca733386f6332427144d379671e89a2985d
Mandeep Jadeja ⚡ <145008273+Mandeep210405@users.noreply.github.com>
2024-08-29 10:30:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/your repo name/g.py",
+    "postStartCommand": "python3 /workspaces/paidfuck/g.py",
     "customizations": {
         "vscode": {
             "settings": {
